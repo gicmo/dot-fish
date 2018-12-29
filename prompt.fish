c30d214692e539108a998357306770f35b709965
Christian Kellner <christian@kellner.me>
2018-12-29 17:41:13 +0100

    prompt: prettify, use user@host for ssh, root

--- prompt.fish
+++ prompt.fish
@@ -1,7 +1,43 @@
 function fish_prompt
-    echo -n ' λ '
-    set_color $fish_color_cwd
+
+    set -l color_cwd $fish_color_cwd
+    set -l color_lambda 949494
+    set -l color_suffix $fish_color_normal
+    set -l color_user $fish_color_user
+
+    set -l print_userhost
+
+    switch "$USER"
+        case root toor
+	    set -l color_root $fish_color_error
+	    set color_suffix $color_root
+	    set color_user $color_root
+	    set print_userhost true
+    end
+
+    if set -q SSH_CONNECTION;
+	set print_userhost true
+    end
+
+    # now actually print the prompt
+
+    if test -n "$print_userhost"
+	set_color $color_user
+	echo -n (whoami)
+	set_color $fish_color_normal
+	echo -n @
+	set_color $fish_color_host
+	echo -n (hostname)
+	set_color $fish_color_normal
+	echo -n " "
+    else
+	set_color $color_lambda
+	echo -n 'λ '
+	set_color $fish_color_normal
+    end
+
+    set_color $color_cwd
     echo -n (prompt_pwd)
-    set_color normal
+    set_color $color_suffix
     echo -n ' → '
 end
