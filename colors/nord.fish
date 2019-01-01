c70d904bc9dceadbf9b346e3352631c3f910bc74
Christian Kellner <christian@kellner.me>
2019-01-01 18:30:00 +0100

    colors: add nord, monokai
    Nord is slightly adapted to my own taste from the gh issue mentioned
    in the file.

---
+++ colors/nord.fish
@@ -0,0 +1,47 @@
+# https://github.com/arcticicestudio/nord/issues/102
+
+set -l nord0  2e3440 # #2e3440
+set -l nord1  3b4252 # #3b4252
+set -l nord2  434c5e # #434c5e
+set -l nord3  4c566a # #4c566a
+set -l nord4  d8dee9 # #d8dee9
+set -l nord5  e5e9f0 # #e5e9f0
+set -l nord6  eceff4 # #eceff4
+set -l nord7  8fbcbb # #8fbcbb
+set -l nord8  88c0d0 # #88c0d0
+set -l nord9  81a1c1 # #81a1c1
+set -l nord10 5e81ac # #5e81ac
+set -l nord11 bf616a # #bf616a
+set -l nord12 d08770 # #d08770
+set -l nord13 ebcb8b # #ebcb8b
+set -l nord14 a3be8c # #a3be8c
+set -l nord15 b48ead # #b48ead
+
+set fish_color_autosuggestion $nord3
+set fish_color_cancel $nord15
+set fish_color_command $nord14 # or 7
+set fish_color_comment $nord5
+set fish_color_cwd $nord6
+set fish_color_end $nord6
+set fish_color_error $nord11
+set fish_color_escape $nord13
+set fish_color_host $nord3
+set fish_color_match $nord8
+set fish_color_normal $nord4
+set fish_color_operator $nord9
+set fish_color_param $nord4
+set fish_color_quote $nord14
+set fish_color_redirection $nord9
+set fish_color_search_match $nord8
+set fish_color_user $nord10
+
+#pager
+set fish_pager_color_prefix $nord13
+set fish_pager_color_completion $nord4
+set fish_pager_color_description $nord10
+set fish_pager_color_progress $nord12
+set fish_pager_color_secondary $nord1
+
+#custom
+
+set fish_color_ck_lambda $nord2
