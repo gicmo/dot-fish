4351e0963c3e5566f4c19ea4c9a2a9cdfc61fce3
Christian Kellner <christian@kellner.me>
2019-01-03 14:36:00 +0100

    config: set env variables

---
+++ config.fish
@@ -0,0 +1,29 @@
+# -=[ fish main config file
+# -=[ © 2019 Christian J. Kellner <christian@kellner.me>
+
+# -=[ environment
+
+set -x EMAIL_ADDRESS "christian@kellner.me"
+
+set -x LC_ALL en_GB.UTF-8
+set -x LANG en_GB.UTF-8
+
+# [ paths and PATH ]
+
+set -x AQUARIUM ~/.config/fish/
+set -x CODE_HOME ~/Code
+
+set -x GOPATH $CODE_HOME/go
+set -x WORKON_HOME $CODE_HOME/envs
+set -x RUST_SRC_PATH $CODE_HOME/src/rust/src
+
+path_add ~/.local/bin
+path_add ~/.cargo/bin $GOPATH/bin
+path_add /usr/local/bin /usr/local/sbin/
+
+# [ editors ]
+
+set -x EDITOR ew
+set -x CVSEDITOR $EDITOR
+set -x VISUAL $EDITOR
+
